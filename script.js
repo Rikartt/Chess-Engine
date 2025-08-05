cd2837d412a2918d43404261e447d740e6e7dd8d
Rikartt <blennhemrikard@gmail.com>
2025-08-05 16:30:21 +0200

    added numbers to the tiles

--- script.js
+++ script.js
@@ -8,6 +8,34 @@ function createGrid (width, height) {
     }
     return retgrid
 }
+const mapNumtoLetter = {
+  0: "A",
+  1: "B",
+  2: "C",
+  3: "D",
+  4: "E",
+  5: "F",
+  6: "G",
+  7: "H",
+  8: "I",
+  9: "J",
+  10: "K",
+  11: "L",
+  12: "M",
+  13: "N",
+  14: "O",
+  15: "P",
+  16: "Q",
+  17: "R",
+  18: "S",
+  19: "T",
+  20: "U",
+  21: "V",
+  22: "W",
+  23: "X",
+  24: "Y",
+  25: "Z"
+};
 function renderGrid (grid, elementid) {
     var c = document.getElementById(elementid);
     var ctx = c.getContext("2d");
@@ -21,9 +49,12 @@ function renderGrid (grid, elementid) {
                 ctx.fillStyle = "black";
             }
             ctx.fillRect(i*tilewidth, j*tileheight, tilewidth, tileheight);
+            ctx.fillStyle="blue"
+            ctx.fillText(`${mapNumtoLetter[i]}${8 - j}`, i*tilewidth+10, (j+1)*tileheight-10, 20)
         }
     }
 }
 let maingrid = createGrid(8,8)
 console.log(maingrid)
+maingrid[5][5] = "K"
 renderGrid(maingrid, "maincanvas")
